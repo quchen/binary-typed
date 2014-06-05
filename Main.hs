61f390775da02355f82936ba178910290cafdc6c
David Luposchainsky <dluposchainsky@gmail.com>
2014-06-06 01:38:37 +0200

    Doc, clean up source file (#3)

--- Main.hs
+++ Main.hs
@@ -1,6 +1,38 @@
-{-# OPTIONS_GHC -Wall #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE DeriveGeneric #-}
+
+-- | Defines a type-safe 'Binary' instance.
+--
+--   Standard 'Binary' serializes to 'Data.ByteString.Lazy.ByteString', which
+--   is an untyped format; deserialization of unexpected input usually results
+--   in unusable data.
+--
+--   This module defines a 'Typed' type, which allows serializing both a value
+--   and the type of that value; deserialization can then check whether the
+--   received data was sent assuming the right type, and error messages
+--   may provide insight into the type mismatch.
+--
+--   Example without type safety:
+--
+--   > test = let val = 'True'
+--   >            enc = 'encode' val
+--   >            dec = 'decode' enc :: 'Int'
+--   >        in  print dec
+--
+--   This behaves unexpectedly: A 'True' value is converted to an 'Int', which
+--   corresponds to a wacky type coercion. The receiving end has no way of
+--   knowing what the incoming data should have been interpreted as.
+--
+--   Using 'Typed', this can be avoided:
+--
+--   > test' = let val = 'True'
+--   >             enc = 'encode' ('typed' 'Full' val)
+--   >             dec = 'decode' enc :: 'Typed' 'Int'
+--   >         in  print dec
+--
+--   This time 'decode' raises an error: the incoming data is tagged as a
+--   'Bool', but is attempted to be decoded as 'Int'.
+
 
 module Mainn (
       Typed
@@ -20,7 +52,6 @@ import qualified Data.Typeable.Internal as TI (Fingerprint(..), TypeRep(..))
 import Data.Binary
 import Control.Applicative
 
---import Test.QuickCheck
 
 
 -- ^ Type information stored alongside a value to be serialized, so that the
@@ -35,7 +66,8 @@ instance Binary TypeInformation
 
 
 
--- | A value suitable to be typechecked using the contained 'TypeInformation'.
+-- | A value suitable to be typechecked using the contained extra type
+--   information.
 data Typed a where
       Typed :: Typeable a => TypeInformation -> a -> Typed a
 
@@ -47,6 +79,15 @@ instance Show a => Show (Typed a) where
 
 
 
+instance (Binary a, Typeable a) => Binary (Typed a) where
+      get = do result <- get
+               case typecheck result of
+                     Left err -> fail   err
+                     Right _  -> return result
+      put (Typed ty value) = put (ty, value)
+
+
+
 -- | Determine how the 'TypeInformation' should be created by 'typed'.
 data TypeFormat =
 
@@ -55,13 +96,16 @@ data TypeFormat =
         --   * Requires only 8 bytes per serialization.
         --   * Subject to false positive due to hash collisions, although in
         --     practice this should almost never happen
-        --     (see "GHC.Fingerprint.Type"for information on hashing).
+        --     (see "GHC.Fingerprint.Type" for information on hashing).
         --   * Type errors cannot tell the expected type ("Expected X, received
         --     type with hash H")
+        --   * Depends on "Data.Typeable.Internal", so the hash function is
+        --     subject to sudden breaking changes. Not recommended for
+        --     future-proof communication.
         Hashed
 
         -- | Compare 'String' representation of types, obtained by calling
-        --   'show' on the 'TypeRep'.
+        --   'show' on the 'Ty.TypeRep'.
         --
         --   * Data size usually between 'Hashed' and 'Full'.
         --   * All types are unqualified, @Foo.X@ and @Bar.X@ look identical,
@@ -89,12 +133,16 @@ typed format x = Typed typeInformation x where
             Shown  -> ShownType  (show           ty)
             Full   -> FullType   (getFull        ty)
 
+
+
 -- | Extract the value of a 'Typed'.
 --
 --   The well-typedness of this is ensured by the 'typed' smart constructor and
 --   the 'Binary' instance of 'Typed'.
 erase :: Typed a -> a
 erase (Typed _ x) = x
+
+
 
 -- | Typecheck a 'Typed'. The result is either the contained value, or an
 --   error message.
@@ -122,10 +170,14 @@ typecheck (Typed typeInformation x) = case typeInformation of
 getFingerprint :: TI.TypeRep -> Fingerprint
 getFingerprint (TI.TypeRep fp _tycon _args) = Fingerprint fp
 
+
+
 -- | 'Ty.TypeRep' without the 'TI.Fingerprint'.
 data TypeRep = TypeRep TyCon [TypeRep]
       deriving (Eq, Ord, Show, Generic)
 instance Binary TypeRep
+
+
 
 -- | 'Ty.TyCon' without the 'TI.Fingerprint'.
 data TyCon = TyCon String -- Package
@@ -133,6 +185,8 @@ data TyCon = TyCon String -- Package
                    String -- Name
       deriving (Eq, Ord, Show, Generic)
 instance Binary TyCon
+
+
 
 -- | Get (only) the representation of a data type, i.e. strip all hashes.
 getFull :: Ty.TypeRep -> TypeRep
@@ -149,6 +203,7 @@ getFull typerep = let (tycon, args) = Ty.splitTyConApp typerep
                                   -- on Typeable.Internal).
 
 
+
 -- | Wrapper around 'TI.Fingerprint' to avoid orphan 'Binary' instance
 newtype Fingerprint = Fingerprint TI.Fingerprint
       deriving (Eq, Ord, Show)
@@ -156,56 +211,3 @@ newtype Fingerprint = Fingerprint TI.Fingerprint
 instance Binary Fingerprint where
       get = liftA2 (\a b -> Fingerprint (TI.Fingerprint a b)) get get
       put (Fingerprint (TI.Fingerprint a b)) = put a *> put b
-
-instance (Binary a, Typeable a) => Binary (Typed a) where
-      get = do result <- get
-               case typecheck result of
-                     Left err -> fail   err
-                     Right _  -> return result
-      put (Typed ty value) = put (ty, value)
-
--- main = do
---       let val = 0 :: Word8
---       print val
---       putStrLn "==================="
---       let enc = encode (Typed val)
---       print enc
---       putStrLn "==================="
---       let dec = decodeOrFail enc :: Either (BSL.ByteString, Get.ByteOffset, String) (BSL.ByteString, Get.ByteOffset, Typed Word8)
---       print dec
-
-
-
-
--- #############################################################################
--- #############################################################################
--- #############################################################################
-
--- instance Arbitrary TI.Fingerprint where
---       arbitrary = liftA2 TI.Fingerprint arbitrary arbitrary
---
--- instance Arbitrary TI.TyCon where
---       arbitrary = TI.TyCon <$> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary
---
--- instance Arbitrary TI.TypeRep where
---       arbitrary = TI.TypeRep <$> arbitrary <*> arbitrary <*> resize 1 arbitrary
---
--- instance (Arbitrary a) => Arbitrary (Typed a) where
---       arbitrary = fmap Typed arbitrary
---
---
--- prop_fingerprint :: TI.Fingerprint -> Bool
--- prop_fingerprint fp = fp == decode (encode fp)
--- prop_tycon :: TI.TyCon -> Bool
--- prop_tycon tc = tc == decode (encode tc)
--- prop_typerep :: TI.TypeRep -> Bool
--- prop_typerep tr = tr == decode (encode tr)
--- prop_typed :: Typed Int -> Bool
--- prop_typed ty = ty == decode (encode ty)
---
---
--- qc = do
---       quickCheck prop_fingerprint
---       quickCheck prop_tycon
---       quickCheck prop_typerep
---       quickCheck prop_typed
